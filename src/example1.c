9c0560c22c94be9901871280b32f891593a2f4f6
Venscor <354951381@qq.com>
2015-11-20 22:00:22 +0800

    试验中的源文件

---
+++ src/example1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void function(int a,int b,int c){
+	char buffer1[5];
+	char buffer2[10];
+	int *ret;
+	ret=(int*)(buffer1+13);
+	(*ret)+=8;
+}
+
+void main(){
+	int x;
+	x=0;
+	function(1,2,3);
+	x=1;
+	printf("%d\n",x);
+}
